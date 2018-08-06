cc253b108d9e7faa398cafbf90b3a4c3da58184c
Trey Hunner <trey@treyhunner.com>
2018-08-06 15:29:13 -0700

    Use new remote_field instead of old rel

--- relatives/templatetags/relatives.py
+++ relatives/templatetags/relatives.py
@@ -30,7 +30,7 @@ def contents_or_fk_link(field):
         return contents
     else:
         model_field, _, _ = lookup_field(field_name, obj, field.model_admin)
-        if getattr(model_field, 'rel', None) and hasattr(related_obj, '_meta'):
+        if model_field.remote_field and hasattr(related_obj, '_meta'):
             try:
                 return mark_safe('<a href="%s">%s</a>' %
                                  (get_admin_url(related_obj), contents))
