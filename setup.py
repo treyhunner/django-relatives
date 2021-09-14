7e23535da81ff0770f9a4164279226b71ccbabc2
Trey Hunner <trey@treyhunner.com>
2021-09-14 14:03:00 -0700

    Fix supported Django version in setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     )),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['Django >= 1.4.2'],
-    tests_require=['Django >= 1.4.2'],
+    install_requires=['Django >= 1.11'],
+    tests_require=['Django >= 1.11'],
     test_suite='runtests.runtests',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
